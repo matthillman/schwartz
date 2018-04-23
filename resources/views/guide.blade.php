f7fb4322d52b9667c238c7a2367d9ebab508f23d
Matthew Hillman <mhillman@bomgar.com>
2018-04-23 11:54:35 -0500

    Fix templates

--- resources/views/guide.blade.php
+++ resources/views/guide.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
     <div class="markdown">
         {!! $content !!}
     </div>
+</div>
 @endsection
--- resources/views/handbook.blade.php
+++ resources/views/handbook.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
     <div class="markdown">
     @foreach($sections as $section)
         <div>{!! $section !!}</div>
     @endforeach
     </div>
+</div>
 @endsection
