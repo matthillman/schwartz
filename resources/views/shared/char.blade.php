d8315ee6ee59243575221b2bcaeb4fe77f151f0e
Matthew Hillman <mhillman@bomgar.com>
2018-07-01 15:56:28 -0500

    Show zetas

--- resources/views/shared/char.blade.php
+++ resources/views/shared/char.blade.php
@@ -1,8 +1,14 @@
 @if (isset($character))
 <span gear="{{ $character->gear_level }}"
     stars="{{ $character->rarity }}"
+    class="character"
 >
-    <span>{{ $character->rarity }}* g{{ $character->gear_level }}</span>
+    <span>
+        <span>{{ $character->rarity }}* g{{ $character->gear_level }}</span>
+        @foreach($character->zetas as $zeta)
+            <span class="zeta">{{ $zeta->class[0] }}</span>
+        @endforeach
+    </span>
 </span>
 @else
 <span missing>
