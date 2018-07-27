dacb01db4be61fe152c99713412c5a1abeb7e86e
Matthew Hillman <mhillman@bomgar.com>
2018-07-27 16:38:37 -0500

    separate queries

--- routes/api.php
+++ routes/api.php
@@ -23,14 +23,27 @@ Route::middleware('client')->get('/tw/compare/{first}/{second}', function (Reque
     $data = DB::table('guilds')
         ->join('members', 'members.guild_id', '=', 'guilds.id')
         ->join('characters', 'characters.member_id', '=', 'members.id')
-        ->join('character_zeta', 'character_zeta.character_id', '=', 'characters.id')
-        ->selectRaw("count('zeta_id') as zetas, sum(case when characters.gear_level = 12 then 1 else 0 end) as gear_12, sum(case when characters.gear_level = 11 then 1 else 0 end) as gear_11, guilds.guild_id")
+        ->selectRaw("sum(case when characters.gear_level = 12 then 1 else 0 end) as gear_12, sum(case when characters.gear_level = 11 then 1 else 0 end) as gear_11, guilds.guild_id")
         ->groupBy('guilds.guild_id')
         ->whereIn('guilds.guild_id', [$guild1->guild_id, $guild2->guild_id])
         ->get();
 
     $g1Data = $data->firstWhere('guild_id', $guild1->guild_id);
     $g2Data = $data->firstWhere('guild_id', $guild2->guild_id);
+
+    $zetas = DB::table('character_zeta')
+        ->join('characters', 'character_id', '=', 'characters.id')
+        ->join('members', 'characters.member_id', '=', 'members.id')
+        ->selectRaw('count(1) as zetas, members.guild_id')
+        ->groupBy('members.guild_id')
+        ->whereIn('members.guild_id', [$guild1->guild_id, $guild2->guild_id])
+        ->get();
+
+    $g1Zetas = $zetas->firstWhere('guild_id', $guild1->guild_id);
+    $g2Zetas = $zetas->firstWhere('guild_id', $guild2->guild_id);
+
+    $g1Data['zetas'] = $g1Zetas['zetas'];
+    $g2Data['zetas'] = $g2Zetas['zetas'];
 
     return response()->json([
         $guild1->name => $g1Data,
