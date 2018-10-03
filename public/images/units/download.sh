dce1db0cca0bf0280fb175597d31dc4eb0964be7
Matthew Hillman <mhillman@bomgar.com>
2018-10-03 12:52:16 -0500

    Update download script ans zombie pic

--- public/images/units/download.sh
+++ public/images/units/download.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 IFS=$' \n'
-declare -a units=(`echo "select base_id from units where combat_type = 'CHARACTER' order by 1;" | psql -t -U schwartz`)
+
+declare -a units
+if [[ $# > 0 ]]; then
+    units=($@)
+else
+    units=(`echo "select base_id from units where combat_type = 'CHARACTER' order by 1;" | psql -t -U schwartz`)
+fi
 
 for unit in "${units[@]}"; do
     echo "Downloading https://api.swgoh.help/image/char/${unit}"
