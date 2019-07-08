9e8cdeb85b88fa86898f85931d32e563ad4612eb
Matthew Hillman <mhillman@bomgar.com>
2019-07-08 15:13:11 -0500

    Really fix it this time

--- routes/api.php
+++ routes/api.php
@@ -97,6 +97,6 @@ Route::middleware('client')->get('/tw/compare/{first}/{second}', function (Reque
         $guild1->name => $g1Data,
         $guild2->name => $g2Data,
         'char_keys' => collect($chars)->map(function($name, $unit) { return strtolower($unit); })->values()->toArray(),
-        'char_names' => $chars
+        'char_names' => collect($chars)->mapWithKeys(function($name, $unit) { return [strtolower($unit) => $name]; })->toArray(),
     ]);
 });
