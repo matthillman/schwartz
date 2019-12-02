f722b8619b7427f8594bf3da881d4c6b644c4235
Matthew Hillman <mhillman@bomgar.com>
2019-12-02 15:19:19 -0600

    Image udpates

--- public/images/units/download.sh
+++ public/images/units/download.sh
@@ -6,12 +6,12 @@ declare -a units
 if [[ $# > 0 ]]; then
     units=($@)
 else
-    units=(`echo "select base_id from units where combat_type = 'CHARACTER' order by 1;" | psql -t -U schwartz`)
+    units=(`echo "select base_id from units order by 1;" | psql -t -U schwartz`)
 fi
 
 for unit in "${units[@]}"; do
-    echo "Downloading https://api.swgoh.help/image/char/${unit}"
-    curl https://api.swgoh.help/image/char/${unit} > ${unit}.png
+    echo "Downloading https://swgoh.gg/game-asset/u/${unit}/"
+    curl https://swgoh.gg/game-asset/u/${unit}/ > ${unit}.png
     convert ${unit}.png -transparent black ${unit}.png
 done
 
