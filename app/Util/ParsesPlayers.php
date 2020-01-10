55a6e80f256bab7657cd23594751c2a1727f028f
Matthew Hillman <mhillman@bomgar.com>
2020-01-09 23:17:25 -0600

    defaults

--- app/Util/ParsesPlayers.php
+++ app/Util/ParsesPlayers.php
@@ -113,8 +113,8 @@ trait ParsesPlayers {
 
         $member->arena = $member_data['arena'];
         $member->level = $member_data['level'];
-        $member->title = array_get($member_data, 'titles.selected', '');
-        $member->portrait = array_get($member_data, 'portraits.selected', '');
+        $member->title = array_get($member_data, 'titles.selected', 'Patron');
+        $member->portrait = array_get($member_data, 'portraits.selected', 'NONE');
 
         // $stats = collect($member_data['stats']);
         // Fucking game
