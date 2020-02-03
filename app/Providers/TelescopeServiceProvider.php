f02141bf952526aa1b004297ee7d6606766acfaa
Matthew Hillman <mhillman@bomgar.com>
2020-02-03 09:21:15 -0600

    Fix telescope auth

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -54,6 +54,32 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     }
 
     /**
+     * Configure the Telescope authorization services.
+     *
+     * @return void
+     */
+    protected function authorization()
+    {
+        $this->gate();
+
+        Telescope::auth(function ($request) {
+            $result = collect(config('auth.guards'))->keys()->reduce(function($auth, $guard) use ($request) {
+                if ($auth) {
+                    return $auth;
+                }
+
+                app('auth')->shouldUse($guard);
+
+                return Gate::check('viewTelescope', [$request->user()]);
+            }, app()->environment('local'));
+
+            app('auth')->shouldUse(config('auth.defaults.guard'));
+
+            return $result;
+        });
+    }
+
+    /**
      * Register the Telescope gate.
      *
      * This gate determines who can access Telescope in non-local environments.
