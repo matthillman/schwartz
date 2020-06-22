d314447137385a1c5984ec6e178c1dd64001edb0
Matthew Hillman <mhillman@bomgar.com>
2020-06-22 10:46:54 -0500

    For real this time

--- resources/js/components/Character.vue
+++ resources/js/components/Character.vue
@@ -63,7 +63,7 @@
       <div class="stat-container" v-if="showStats">
         <div v-for="(stat, key) in character.key_stats" :key="key" class="stat-wrapper">
           <span class="stat" :grade="character.stat_grade[key]">
-            <span>{{ formatStat(stat[1], stat[0]).toLocaleString() }}</span>
+            <span>{{ formatStat(stat[1], key).toLocaleString() }}</span>
             <span class="mod-set-image tier-5 mini" :class="[stat[0]]"></span>
           </span>
         </div>
@@ -133,7 +133,7 @@ const percent_stats = [
 ];
 
 function getStatKey(value) {
-  return Object.keys(UnitStat).find(key => UnitStat[key] === value);
+  return Object.keys(UnitStat).find(key => UnitStat[key] == value);
 }
 
 function range(size, startAt = 0) {
