84e9569c57bd5998cc1d17aa8e16df4b0a85c720
Matthew Hillman <mhillman@bomgar.com>
2021-02-17 20:57:56 -0600

    Log env

--- resources/bot-2/src/bot.ts
+++ resources/bot-2/src/bot.ts
@@ -23,6 +23,7 @@ export class Bot {
     ) { }
 
     public async listen(): Promise<string> {
+        console.log(`[BOT] Starting bot in env [${Environment[this.settings.config.env]}]`);
         this.client.on('ready', async () => await this.onReady());
         this.client.on('message', async message => await this.onMessage(message));
         this.client.on('guildCreate', async guild => await this.onGuildCreate(guild));
