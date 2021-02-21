a52578b2293ef439565b4f5a2d2d1acfac228df4
Matthew Hillman <mhillman@bomgar.com>
2021-02-20 22:43:50 -0600

    Error printing?

--- resources/bot-2/src/index.ts
+++ resources/bot-2/src/index.ts
@@ -14,9 +14,10 @@ const dbPool = container.get<Pool>(TYPES.DBPool);
 const broadcastProvider = container.get<BroadcastProvider>(TYPES.BroadcastProvider);
 
 // These 2 process methods will catch exceptions and give *more details* about the error and stack trace.
-process.on('uncaughtException', (err) => {
+process.on('uncaughtException', (err, origin) => {
     const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
     console.error(`Uncaught Exception: ${errorMsg}`);
+    console.error(`Exception origin: ${origin}`);
     // Always best practice to let the code crash on uncaught exceptions.
     // Because you should be catching them anyway.
     process.exit(1);
@@ -24,6 +25,10 @@ process.on('uncaughtException', (err) => {
 
 process.on('unhandledRejection', err => {
     console.error(`Unhandled rejection: ${err}`);
+    if (err instanceof Error) {
+        const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
+        console.error(`\tLine: ${errorMsg}`);
+    }
 });
 
 async function init() {
